13fe750dfbb2d19c53ff871558dfbcc26013c567
opohass <yoavkitai@gmail.com>
2022-10-06 17:59:05 +0300

    Finished First Batch Of Questions And Modified Process

--- text_to_jason.py
+++ text_to_jason.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 # Enter Text Below
 # <EXAMPLE> IS TO BE REPLACED
@@ -9,23 +10,46 @@ import json
 # [<ANSWER_B>, False],
 # [<ANSWER_C>, False],
 # [<ANSWER_D>, False],
+# [<ANSWER_E (You Can Leave Empty If There Is No Option)>, False],
+# [<ANSWER_F (You Can Leave Empty If There Is No Option)>, False],
+# [<ANSWER_G (You Can Leave Empty If There Is No Option)>, False],
+# [<ANSWER_H (You Can Leave Empty If There Is No Option)>, False],
+# <IMAGE NAME> or False
 # ]
 
 text_to_add = [
+    # Question 29
     "",
-    ["", True],
     ["", False],
     ["", False],
-    ["", False]
+    ["", False],
+    ["", False],
+    ["", False],
+    ["", False],
+    ["", False],
+    ["", False],
+    False
 ]
-
-f = open('/Users/yoavkitai/Documents/Bynet/GCP/quiz_proj/ml_questions.json', 'r')
+path = os.getcwd() + "/quiz_data/ml_questions.json"
+f = open(path, 'r')
 data = json.load(f)
 f.close()
-for i in range(0, len(text_to_add), 5):
+for i in range(0, len(text_to_add), 10):
     next_pos = str(int(list(data["questions"].keys())[-1]) + 1)
+    print()
     data["questions"][next_pos] = text_to_add[i]
     data['answers'][next_pos] = {"A":text_to_add[i+1], "B":text_to_add[i+2], "C":text_to_add[i+3], "D":text_to_add[i+4]}
-f = open('/Users/yoavkitai/Documents/Bynet/GCP/quiz_proj/ml_questions.json', 'w')
+    if text_to_add[i+5][0] != "":
+        data["answers"][next_pos]["E"] = text_to_add[i+5]
+        if text_to_add[i+6][0] != "":
+            data["answers"][next_pos]["F"] = text_to_add[i+6]
+            if text_to_add[i+7][0] != "":
+                data["answers"][next_pos]["G"] = text_to_add[i+7]
+                if text_to_add[i+8][0] != "":
+                    data["answers"][next_pos]["H"] = text_to_add[i+8]
+        
+    data["images"][next_pos] = text_to_add[i+9]
+        
+f = open(path, 'w')
 json.dump(data, f, indent=4)
 f.close()
